38ba8ace0ce0ec9b11f1bb250fb75053dea2b04b
miriam <miiriampc@gmail.com>
2017-06-04 13:03:40 -0500

    correcting name functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,21 +8,21 @@ $(document).ready(function(){
   var nextImg = function(){
     currentImg = indexImg+1;
     if (currentImg > ulChild.length) {currentImg = 1}
-    printImage(currentImg);
+    showCarousel(currentImg);
   }
 
   var prevImg = function(){
     currentImg = indexImg-1;
     if (currentImg < 1) {currentImg = ulChild.length}
-    printImage(currentImg);
+    showCarousel(currentImg);
   }
 
   var dot = function(){
     var idImg=$(this).attr('data-id');
-    printImage(idImg);
+    showCarousel(idImg);
   }
 
-  function printImage(position){
+  function showCarousel(position){
     $(".container ul li:nth-child("+ indexImg +")").hide();
     $(".container ul li:nth-child("+ position +")").show();
     $(".container span:nth-child("+indexImg+")").removeClass('js-active');
@@ -30,9 +30,9 @@ $(document).ready(function(){
     indexImg = position;
   }
 
-  (function showCarousel(){
+  (function automaticCarousel(){
     nextImg();
-    setTimeout(showCarousel, 5000);
+    setTimeout(automaticCarousel, 5000);
   }());
 
   $(".next").on('click',nextImg);
